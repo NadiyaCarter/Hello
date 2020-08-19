5610f059d0d90f9422a85522d85994c4bd6c0d45
NadiyaCarter <69914072+NadiyaCarter@users.noreply.github.com>
2020-08-19 11:59:18 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,18 @@
+/***
+*Hello.java
+***
+*Nadiya Carter
+*August 19, 2020
+*CMSC 255 004
+***/
+
+public class Hello{
+	public static void main(String[] args){
+		System.out.println("<Nadiya Carter>");
+		System.out.println("<Hello.java>");
+		System.out.println("<Printing out statements>");
+		System.out.println("<8/19/2020>");
+		System.out.println("<CMSC 255 004>");
+		System.out.println();
+	}
+}
